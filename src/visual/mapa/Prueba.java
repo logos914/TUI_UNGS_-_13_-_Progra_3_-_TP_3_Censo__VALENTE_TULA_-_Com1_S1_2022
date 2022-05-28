e5ba31e1547599b337bdfc8ab05ea1d86b21450d
Ignacio Tula <itula@logos.net.ar>
2022-05-28 03:36:41 -0300

    una prueba de mapa muy rápída

---
+++ src/visual/mapa/Prueba.java
@@ -0,0 +1,55 @@
+package visual.mapa;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+import org.openstreetmap.gui.jmapviewer.Coordinate;
+import org.openstreetmap.gui.jmapviewer.JMapViewer;
+
+public class Prueba {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Prueba window = new Prueba();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Prueba() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JMapViewer mapa = new JMapViewer();
+	
+		
+		frame.add(mapa);
+		
+		Coordinate coordenadaInicial= new Coordinate(-34.52202,-58.70002);
+		
+		
+		mapa.setLayout(null);
+	}
+
+}
