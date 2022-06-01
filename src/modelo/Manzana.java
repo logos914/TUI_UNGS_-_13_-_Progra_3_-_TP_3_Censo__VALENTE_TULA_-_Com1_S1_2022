4372ac2e69986e2e0667a0d0c1042a49e10aaddb
Ignacio Tula <itula@logos.net.ar>
2022-06-01 03:31:28 -0300

    Update Manzana.java

--- src/modelo/Manzana.java
+++ src/modelo/Manzana.java
@@ -1,99 +1,48 @@
 package modelo;
 
-import org.openstreetmap.gui.jmapviewer.Coordinate;
-import org.openstreetmap.gui.jmapviewer.MapPolygonImpl;
+import org.locationtech.jts.geom.Coordinate;
+
+import modelo.georreferenciable.geometrico.PoligonoDeCuatroLados;
 
 public class Manzana {
 
-	private Coordinate geoLocalizacion;
-	private Censita censitaAsignado;
-	private MapPolygonImpl geoRectangulo;
-
-	public Manzana(Coordinate geoLocalizacion) {
-		this.geoLocalizacion = geoLocalizacion;
+	
+	private Censista censistaAsignado;
+	private PoligonoDeCuatroLados poligonoGeografico;
+	private Coordinate[] coordenadasDeAristas;
+	
+	private Manzana(Coordinate[] coordenadasDeAristas) {
+		this.coordenadasDeAristas = coordenadasDeAristas;
 	}
 
-	public static Manzana soloCoordenada(Coordinate geoLocalizacion) {
-		return new Manzana(geoLocalizacion);
+	public static Manzana manzana(float[][] aristas) {
+		Manzana manzana = new Manzana(conversionTipoDeCoordenadas (aristas));
+		return manzana; 
+	}
+	
+	public static Manzana manzana(Coordinate[] aristas) {
+		Manzana manzana = new Manzana(aristas);
+		return manzana; 
+	}
+	
+	private static Coordinate[]  conversionTipoDeCoordenadas (float[][] aristas) {
+		Coordinate[] aristasComoCoordenadas = new Coordinate[aristas.length];
+	    int contador = 0;
+	    for (float[] e : aristas) {
+	    	aristasComoCoordenadas[contador] = new Coordinate(e[0], e[1]);
+        	contador++;
+        }
+	    return aristasComoCoordenadas;
+	}
+	
+	public void asignarCensista(Censista censista) {
+		this.censistaAsignado = censista;
 	}
 
-	public static Manzana conRectangulo(Coordinate geoLocalizacion, MapPolygonImpl geoRectangulo) {
-		Manzana manzana = new Manzana(geoLocalizacion);
-		manzana.creargeoRectangulo(geoRectangulo);
-		return manzana;
+	private void crearPoligonoDeCuatroLados() {
+		this.poligonoGeografico = new PoligonoDeCuatroLados(this.coordenadasDeAristas);
 	}
 
-	public void asignarCensista(Censita censista) {
-		this.censitaAsignado = censista;
-	}
-
-	public void creargeoRectangulo(MapPolygonImpl geoRectangulo) {
-		this.geoRectangulo = geoRectangulo;
-	}
-
-	public Coordinate getGeoLocalizacion() {
-		return geoLocalizacion;
-	}
-
-	public Censita getCensitaAsignado() {
-		return censitaAsignado;
-	}
-
-	public MapPolygonImpl getGeoRectangulo() {
-		return geoRectangulo;
-	}
-
-	public RumboCardinal dondeSeUbicaConRespecto(Manzana otraManzana) {
-
-		// Primero evaluamos Lat, y dentro de ella Long
-
-		// Al Norte
-		if (this.estaAlNorteDe(otraManzana.getGeoLocalizacion())) {
-			if (this.estaAlOesteDe(otraManzana.getGeoLocalizacion())) {
-				return RumboCardinal.NOROESTE;
-			} else if (this.estaAlEsteDe(otraManzana.getGeoLocalizacion())) {
-				return RumboCardinal.NORESTE;
-			} else {
-				return RumboCardinal.NORTE;
-			}
-
-		// Al Sur
-		} else if (this.estaAlSurDe(otraManzana.getGeoLocalizacion())) {
-			if (this.estaAlOesteDe(otraManzana.getGeoLocalizacion())) {
-				return RumboCardinal.SUROESTE;
-			} else if (this.estaAlEsteDe(otraManzana.getGeoLocalizacion())) {
-				return RumboCardinal.SURESTE;
-			} else {
-				return RumboCardinal.SUR;
-			}
-
-		// Misma Latitud
-		} else {
-			if (this.estaAlOesteDe(otraManzana.getGeoLocalizacion())) {
-				return RumboCardinal.OESTE;
-			} else if (this.estaAlEsteDe(otraManzana.getGeoLocalizacion())) {
-				return RumboCardinal.ESTE;
-			} else {
-				return RumboCardinal.IGUAL;
-			}
-		}
-
-	}
-
-	public boolean estaAlNorteDe(Coordinate coordenada) {
-		return this.geoLocalizacion.getLat() > coordenada.getLat() ? true : false;
-	}
-
-	public boolean estaAlSurDe(Coordinate coordenada) {
-		return this.geoLocalizacion.getLat() < coordenada.getLat() ? true : false;
-	}
-
-	public boolean estaAlEsteDe(Coordinate coordenada) {
-		return this.geoLocalizacion.getLon() > coordenada.getLon() ? true : false;
-	}
-
-	public boolean estaAlOesteDe(Coordinate coordenada) {
-		return this.geoLocalizacion.getLon() < coordenada.getLon() ? true : false;
-	}
+	
 
 }
