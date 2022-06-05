460b1a8e0a36d0c5d60b6f6e92ee268e8de153bf
Ignacio Tula <itula@logos.net.ar>
2022-06-05 20:58:09 -0300

    Refactor

--- src/modelo/georreferenciable/geometrico/PoligonoDeCuatroLados.java
+++ src/modelo/georreferenciable/geometrico/PoligonoDeCuatroLados.java
@@ -1,8 +1,5 @@
 package modelo.georreferenciable.geometrico;
 
-
-
-
 import java.util.ArrayList;
 
 import org.locationtech.jts.geom.Coordinate;
@@ -11,86 +8,58 @@ import org.locationtech.jts.geom.LinearRing;
 import org.locationtech.jts.geom.Point;
 import org.locationtech.jts.geom.Polygon;
 
-
 public class PoligonoDeCuatroLados {
-    
-
     
     private Polygon poligono;
    
+   
     public PoligonoDeCuatroLados(ArrayList<Coordinate> coordenadasDeAristas) {
        
-        
-        if (parametroDeCreacionCorrecto(coordenadasDeAristas)) {
-            
-            
-          
+        if (parametroDeCreacionCorrecto(coordenadasDeAristas)) 
+        {
+            Coordinate[] coordenadasParaConstruirPoligono = PoligonoDeCuatroLados.convertirArrayListEnArreglo(coordenadasDeAristas);
             GeometryFactory fact = new GeometryFactory();
-            
-            Coordinate[] coordenadasParaConstruirPoligono = new Coordinate[coordenadasDeAristas.size()];
-            
-            for (int i = 0; i<coordenadasDeAristas.size();i++) {
-            	coordenadasParaConstruirPoligono[i] = coordenadasDeAristas.get(i);
-            }
-            
-            
             LinearRing linear = new GeometryFactory().createLinearRing(coordenadasParaConstruirPoligono);
             
             this.poligono = new Polygon(linear, null, fact);
-      
-            
-   
-        } else {
+
+        } 
+        else 
+        {
             throw new IllegalArgumentException("El poligono debe tener cuatro aristas, " + 
-            "o cinco si la arista final es igual a la primera. \n Dato invalido: " + coordenadasDeAristas);
+            "y cinco vértices, dónde el último debe ser igual al primero para cerrar el polígono. \n "
+            + "Cantidad de aristas:: " + coordenadasDeAristas.size() + "\n" 
+            + "Aristas: " + coordenadasDeAristas);
         }
-
-
-
     }
 
 
-   
-
-
+   private static Coordinate[] convertirArrayListEnArreglo(ArrayList<Coordinate> aristas) {
+	   Coordinate[] arregloDeCoordenadas= new Coordinate[aristas.size()];  
+	   for (int i = 0; i<aristas.size();i++) 
+	   {
+		   arregloDeCoordenadas[i] = aristas.get(i);
+       }
+	   return arregloDeCoordenadas;
+   }
 
     private static boolean  parametroDeCreacionCorrecto(ArrayList<Coordinate> aristas) {
-
-        if (aristas.size() == 4) {
-            return true;
-        }
-
-        if (aristas.size() == 5) {
-            return esUnCircuitoCerradoDeCincoAristas(aristas);
-        }
-
-        return false;
-
+        if (aristas.size() == 5) { return esUnCircuitoCerradoDeCincoAristas(aristas);} 
+        else {  return false; }
     }
 
-    
     private static boolean esUnCircuitoCerradoDeCincoAristas(ArrayList<Coordinate> aristas) {
             return (aristas.get(0).equals(aristas.get(4))) ? true : false;
         }
-
     
     public Coordinate[] obtenerCoordenadas() {
     	return this.poligono.getCoordinates();
     }
 
-    
-    
     public boolean contiene (Coordinate coordenadas) {
-    
     	GeometryFactory fact = new GeometryFactory();
     	Point punto = fact.createPoint(coordenadas);
     	return this.poligono.contains(punto);
     }
-        
-
-
-
-   
-
-  
+       
 }
