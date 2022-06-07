8f257a822e8ea74cb55f15f26faeda2e4d27f959
FrancoVal <73122553+FrancoVal@users.noreply.github.com>
2022-06-06 22:55:24 -0300

    Refactoring "Censo"

--- src/censo/Censo.java
+++ src/censo/Censo.java
@@ -8,41 +8,8 @@ import controlador.Controlador;
 import frontend.Frontend;
 
 public class Censo {
-
-	
-
-	
 	public static void main(String[] args) {
-		
-		
-		try {
-			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		} catch (Exception e) {
-			System.out.print("No se puede iniciar el apartado gráfico de la aplicación\n Problema: " + e);
-		}
-		
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Controlador controlador =  new Controlador();
-					Frontend gui = new Frontend(controlador);
-				
-					
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-		
-		
-		
-		
-		
-		
-	}
-	
-	
-	}
-
-
-
+		Controlador controlador =  new Controlador();
+		Frontend gui = new Frontend(controlador);
+	}	
+}
